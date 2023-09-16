5121ebe667a7f873cee6ec03105f8c88aafc37fc
bobur <boburmakhmadkulov05@gmail.com>
2023-09-16 12:46:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// let name = prompt('What is your name?').toLowerCase().trim()
+// let account = +prompt('Enter your bank PIN.')
+
+// if (name === 'alex' && account === 7777) {
+    
+//     let money = prompt('How much to cash out?')
+//     if (money <= 10000 && money > 0) {
+//         alert('Next!')
+//     } else {
+//         alert('You do not have enough money!')
+//     }
+    
+// } else {
+//     alert('User is not found!')
+// }
+
+
+
+let name = prompt('What is your name?').toLowerCase().trim()
+if (name === 'alex') {
+
+    let account = +prompt('Enter your bank PIN.')
+    if(account === 7777) {
+        
+        let money = prompt('How much to cash out?')
+        if (money <= 10000 && money > 0) {
+            alert('Everything is fine!')
+            alert(10000 - money)
+        } else {
+            alert('You do not have enough money!')
+        }
+
+    } else {
+        alert('User is not found!')
+    }
+
+} else {
+    alert('User is not found!')
+}
