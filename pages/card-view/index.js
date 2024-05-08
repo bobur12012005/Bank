2137ccdc95bff8756f1929db8d5bf928041c65b0
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-05-08 10:47:29 +0500

    card-view is done

--- pages/card-view/index.js
+++ pages/card-view/index.js
@@ -1,16 +1,16 @@
-// const box = document.querySelector('.box')
+const card = document.querySelector('.card')
 
-// box.addEventListener('mousemove', (event) => {
-//     const rect = box.getBoundingClientRect()
-//     const x = event.clientX - rect.left
-//     const y = event.clientY - rect.top
+card.addEventListener('mousemove', (event) => {
+    const rect = card.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
 
-//     const rotateX = (y / rect.height - 0.5) * 40
-//     const rotateY = (x / rect.width - 0.5) * -40
+    const rotateX = (y / rect.height - 0.5) * 40
+    const rotateY = (x / rect.width - 0.5) * -40
 
-//     box.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
-// })
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+})
 
-// box.addEventListener('mouseleave', () => {
-//     box.style.transform = ''
-// })
+card.addEventListener('mouseleave', () => {
+    card.style.transform = ''
+})
