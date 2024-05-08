327817cb33315e495b4529d126eaa0a8dda60940
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-05-08 14:01:25 +0500

    adding function to card

--- pages/card-view/index.js
+++ pages/card-view/index.js
@@ -1,5 +1,9 @@
 const card = document.querySelector('.card')
 
+card.addEventListener('dblclick', function () {
+    card.classList.toggle('is-flipped')
+})
+
 card.addEventListener('mousemove', (event) => {
     const rect = card.getBoundingClientRect()
     const x = event.clientX - rect.left
